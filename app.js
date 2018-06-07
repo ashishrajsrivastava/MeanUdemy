c8e85a788b1fe43636bc72b586c7c80daa075ec9
Ashish Raj Srivastava <ashishrajsrivastava@yahoo.in>
2018-06-08 01:04:43 +0530

    change to port 80

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(function(req,res,next){
 app.use(express.static(path.join(__dirname,'public')))
 app.use('/api',routes);
 
-app.set('port',3000);
+app.set('port',80);
 
 
 var server = app.listen(app.get('port'),function(){
